6247ff1c5a0b75c7fdcca3d13d009f4560fa988e
iwu paschal <iwuchukwuezugo@gmail.com>
2023-08-18 23:41:17 +0100

    version 1

---
+++ port.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector('.wrapper');
+const btnPopup = document.querySelector('.btnLogin');
+const iconClose = document.querySelector('.icon-close');
+
+btnPopup.addEventListener('click', ()=> {
+  wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+  wrapper.classList.remove('active-popup');
+});
+function sendEmail(){
+      Email.send({
+        Host : "smtp.gmail.com",
+        Username : "iwuchukwuezugo@gmail.com",
+        Password : "@Phantom23",
+        To : 'iwuchukwuezugo@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "Name:" + document.getElementById("name").value
+        + "<br> Email:" + document.getElementById("email").value
+        + "<br> phone no:" + document.getElementById("phone").value
+        + "<br> Message:" + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Succesfully")
+    );
+}
